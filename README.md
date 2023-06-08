de92671a101b650bf40af2b1f92f48a976337ea6
jgansl <jessegansler@gmail.com>
2023-06-07 21:18:38 -0700

    oai integration

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 
 [Notion API Dashboard](https://www.notion.so/my-integrations/internal/5a223e4be9c241538dc8868e55bcddac)
+[OAI CLI](https://github.com/openai/openai-python#command-line-interface)
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ load_dotenv()
 import openai
 openai.api_key = environ["OPENAI_API_KEY"]
 
-from notion_client import Client#,AsyncClient
+import logging
+from notion_client import Client, APIErrorCode, APIResponseError#,AsyncClient
 notion = Client(auth=environ["NOTION_TOKEN"])
 # notion = AsyncClient(auth=environ["NOTION_TOKEN"])
 
@@ -15,7 +16,15 @@ models = openai.Model.list()
 print(models.data[0].id)
 
 # create a chat completion
-chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
+chat_completion = openai.ChatCompletion.create(
+   model="gpt-3.5-turbo", 
+   messages=[
+      {
+         "role": "user", 
+         "content": "Hello world"
+      }
+   ]
+)
 
 # print the chat completion
 print(chat_completion.choices[0].message.content)
